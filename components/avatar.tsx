fe1388ecb35a93e89b2a46079d449994ed019bc1
TokiyaHorikawa <ayikot4423@gmail.com>
2020-08-02 21:05:38 +0900

    update: アイコンを丸くしてみる

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -6,7 +6,7 @@ type Props = {
 const Avatar = ({ name, picture }: Props) => {
   return (
     <div className="flex items-center">
-      <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
+      <img src={picture} className="rounded-full h-16 w-16 flex items-center justify-center" alt={name} />
       <div className="text-xl font-bold">{name}</div>
     </div>
   )
